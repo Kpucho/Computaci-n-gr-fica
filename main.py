b2c62c5de75d623fa953676aea5ad32002aa1c89
Juan Diego Campuzano <diegocampuzano62@gmail.com>
2020-04-05 14:18:27 -0500

    jaja salu2

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print "hello world"
+import pygame
+
+pygame.init()
+ventana = pygame.display.set_mode([600, 450])
+fin = False
+
+
+if __name__ == '__main__':
+    while not fin:
+        pygame.display.flip()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                fin = True
