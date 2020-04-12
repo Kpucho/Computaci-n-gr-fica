eb6e52524e4f485b99b3f9b481179578db89828c
Victor <roymason11v1@gmail.com>
2020-04-12 13:24:04 -0500

    Version 0.12 Documentacion

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ import pygame
 from plano import *
 
 
+
+# rotarAng ---> int
+#   angulo de cuanto se mueve la figura con respecto a su posicion original
 def dibujarfigura(rotarAng):
 
-    #Figura lateral-Perfil-------------------------
+    #-----------------POLIGONOS DE LA VISTA LATERAL O PERFIL
+    #Define los puntos de cada poligono o cara
     #figura L
     a1 = [0, 0, 0]
     a2 = [0, 160, 0]
@@ -12,6 +16,8 @@ def dibujarfigura(rotarAng):
     a4 = [0, 120, 100]
     a5 = [0, 120, 40]
     a6 = [0, 0, 40]
+
+    #Define la lista de puntos que conforma el poligono o cara
     acara = [a1, a2, a3, a4, a5, a6]
 
     #mitad grande
@@ -28,7 +34,7 @@ def dibujarfigura(rotarAng):
     k4 = [80, 120, 100]
     kcara = [k1, k2, k3, k4]
 
-    #Figura frontal-------------------------------------------
+    #----------------POLIGONOS DE LA VISTA FRONTAL O ALZADO
     #figura 1.1 pequenita
     b1 = [0, 0, 0]
     b2 = [0, 0, 40]
@@ -61,8 +67,7 @@ def dibujarfigura(rotarAng):
     e8 = [100, 120, 40]
     ecara = [e1, e2, e3, e4, e5, e6, e7, e8]
 
-    #--------------------------------------------------------
-    #plano superior
+    #----------------POLIGONOS DE LA VISTA SUPERIOR O PLANTA
     #figura
     f1 = [0, 120, 100]
     f2 = [0, 160, 100]
@@ -98,15 +103,26 @@ def dibujarfigura(rotarAng):
 
     #====================FIGURA================================
     #ListaCaras
+    #Lista de caras con excepciones
+    #   Es decir: se tiene que dibujar primero para no afectar el dibujo
     listCarasFrontalExc = [dcara]
     listCarasLateralExc = [jcara]
     listCarasSuperiorExc = [hcara]
+    # Las demas listas de caras o poligonos
     listCarasFrontal = [bcara, ccara, ecara]
     listCarasLateral = [acara, kcara]
     listCarasSuperior = [fcara, gcara, icara]
 
-    #Dibujar figura isometrica
+
+
+
+    #====================DIBUJAR FIGURA ISOMETRICA=============================
+
+    #Ang---> vector ang[0], ang[1]
+    #   angulos para la perspectiva, ademas se suma el angulo de rotacion por si
+    #   se mueve la figura
     ang = [30 + rotarAng, 150 + rotarAng]
+
     dibujarCaras(ventana, listCarasFrontalExc, AZUL, ang)
     dibujarCaras(ventana, listCarasLateralExc,  VERDE, ang)
     dibujarCaras(ventana, listCarasSuperiorExc, AMARILLO, ang)
@@ -117,42 +133,74 @@ def dibujarfigura(rotarAng):
     #====================Vistas===========================
 
     #VISTA SUPERIOR
+    #No importa el orden de la lista
     dibujarVistaSuperior([fcara, gcara, icara, hcara], rotarAng)
 
     #VISTA LATERAL
+    #listas de caras organizadas con prioridad
+    #los colores van en paralelo a la lista de caras
     colores = [VERDE, VERDE, AZUL, AZUL, AZUL, AZUL, VERDE]
     dibujarVistaLateral([kcara, jcara, dcara, ecara, bcara, ccara, acara], rotarAng, colores)
 
     #VISTA FRONTAL
+    #listas de caras organizadas con prioridad
+    #los colores van en paralelo a la lista de caras
     colores =[AZUL, AZUL, AZUL, VERDE, VERDE, VERDE, AZUL]
     dibujarVistaFrontal([dcara, ccara, ecara, acara, jcara, kcara, bcara], rotarAng, colores)
 
 
+# =================FUNCIONES PARA LA VISTA SUPERIOR=============================
+
+#Transforma un poligono superior de coordenadas 3d a 2d
+#listaPuntos ---> lista de puntos del poligono o cara
+#ang ---> es el angulo de rotacion (Por si se mueve la figura, cambia las vistas)
+#return ---> listas de puntos ya en coordenadas 2d pantalla
 def aplanarZ(listaPuntos, ang):
     listanueva = []
     for punto in listaPuntos:
+        #rotar figura con un punto fijo por que la vista superior nunca cambia
+        #aprovechar esa ventaja
         puntonuevo = RotarAntiHorario(PuntoPantalla([punto[0], punto[1]]), ang)
         puntonuevo = Traslacion(puntonuevo, [750, 210])
         listanueva.append(puntonuevo)
     return listanueva
 
+#Dibuja la caras superiores
+#caras ---> lista de poligonos o caras superiores
+#ang -----> entero, angulo de rotacion
 def dibujarVistaSuperior(caras, ang):
     for cara in caras:
         caranueva = aplanarZ(cara, ang)
         pygame.draw.polygon(ventana, AMARILLO, caranueva)
         pygame.draw.lines(ventana, NEGRO, True, caranueva)
 
+
+
+# =================FUNCIONES PARA LA VISTA LATERAL=============================
+
+#Transforma un poligono lateral o frontal de coordenadas 3d a 2d (en plano lateral)
+#listaPuntos ---> lista de puntos del poligono o cara
+#ang ---> es el angulo de rotacion (Por si se mueve la figura, cambia las vistas)
+#return ---> listas de puntos ya en coordenadas 2d pantalla
 def aplanarX(listaPuntos, ang):
     listanueva = []
+    #angulos para la transformacion
     a = np.radians(180 + ang)
     b = np.radians(ang)
     for punto in listaPuntos:
+        #Transformar la coordenada y, x en una sola, que seria x
         x = int(punto[1]*np.cos(a)) - int(punto[0]*np.sin(b))
+        # Y = punto[2] o Y = z, ya que la altura no se ve afectada
         puntonuevo = PuntoPantalla([x, punto[2]])
         puntonuevo = Traslacion(puntonuevo, [910, 500])
         listanueva.append(puntonuevo)
     return listanueva
 
+#Dibuja la caras que se ven en la vista lateral
+#caras ---> lista de poligonos o caras tanto laterales y superiores
+#       Estas estan organizadas en un especifico para que quede bien dibujado
+#ang -----> entero, angulo de rotacion
+#colores ----> una lista lista de colores que van en paralelo a lista de poligonos o caras
 def dibujarVistaLateral(caras, ang, colores):
     i=0
     for cara in caras:
@@ -161,8 +209,16 @@ def dibujarVistaLateral(caras, ang, colores):
         pygame.draw.lines(ventana, NEGRO, True, caranueva)
         i+=1
 
+
+# =================FUNCIONES PARA LA VISTA FRONTAL=============================
+
+#Transforma un poligono lateral o frontal de coordenadas 3d a 2d (en plano frontal)
+#listaPuntos ---> lista de puntos del poligono o cara
+#ang ---> es el angulo de rotacion (Por si se mueve la figura, cambia las vistas)
+#return ---> listas de puntos ya en coordenadas 2d pantalla
 def aplanarY(listaPuntos, ang):
     listanueva = []
+    #Angulo necesario para la transformacion
     a = np.radians(ang)
     for punto in listaPuntos:
         x = int(punto[0]*np.cos(a)) - int(punto[1]*np.sin(a))
@@ -171,6 +227,12 @@ def aplanarY(listaPuntos, ang):
         listanueva.append(puntonuevo)
     return listanueva
 
+
+#Dibuja la caras que se ven en la vista lateral
+#caras ---> lista de poligonos o caras tanto laterales y superiores
+#       Estas estan organizadas en un especifico para que quede bien dibujado
+#ang -----> entero, angulo de rotacion
+#colores ----> una lista lista de colores que van en paralelo a lista de poligonos o caras
 def dibujarVistaFrontal(caras, ang, colores):
     i=0
     for cara in caras:
@@ -179,12 +241,26 @@ def dibujarVistaFrontal(caras, ang, colores):
         pygame.draw.lines(ventana, NEGRO, True, caranueva)
         i+=1
 
+
+# Dibuja toda la figura mediante una listas de poligonos o pequenas caras
+# estos poligonos estan conformados por puntos
+# ventana ---> en donde se va a dibujar
+# caras ----> Lista de poligonos ej: listas de Caras Superior
+# color -----> color que identifica esa lista de caras
+# ang -----> vector ang[0], ang[1] (para dar perpectiva a la figura)
 def dibujarCaras(ventana, caras, color, ang):
     for cara in caras:
         caranueva = listPuntos3dA2d(cara, ang)
         pygame.draw.polygon(ventana, color, caranueva)
         pygame.draw.lines(ventana, NEGRO, True, caranueva)
 
+#Transforma una lista de puntos 3d a 2d
+# list3d ---> lista de puntos 3d (Cada uno esta en coordenadas cartesianas)
+# ang ----> es un vector ang[0] y ang[1], Es para intentar darle perspectiva
+#   ang[0] ----> este suele ser el de 30 + anguloRotacion
+#   ang[1] ----> este suele ser el de 150 o 135 (ustedes eligen) + anguloRotacion
+#     Sin embargo estos angulos se definen justo antes de llamar la funcion dibujarcaras
+# return ---> retorna una lista de puntos 2d en coordenadas pantalla
 def listPuntos3dA2d(list3d, ang):
     list2d = []
     for punto3d in list3d:
@@ -192,6 +268,10 @@ def listPuntos3dA2d(list3d, ang):
         list2d.append(punto2d)
     return list2d
 
+# Transforma un punto 3d a 2d
+# v ----> v[0] = x,  v[1] = y,  v[2]= z (coordenadas 3d cartesianas)
+# ang---> vector de angulos para dar perspectiva
+#return retorna coordenadas 2d pantalla
 def p3dA2d(v, ang):
     a = np.radians(ang[0])
     b = np.radians(ang[1])
